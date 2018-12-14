a06018b64454b20b3ce692eee9188cc9f0d18884
thomashli <thomasli537@gmail.com>
2018-12-13 20:59:46 -0800

    tune up

---
+++ chord_detector.py
@@ -0,0 +1,13 @@
+from recording_function import *
+from nd import *
+
+import sys, select, time
+
+#count = 0
+recorder("lala.wav")
+tuner("lala.wav")
+	#print(count, "seconds passed")
+	#count += 1
+	#i,o,e = select.select([sys.stdin],[],[],0.0001)
+	#if i == [sys.stdin]:
+	#	break
